4eec4d48d299094a046b00b4b04f7fef5ae15988
Monk <71825876+dwbai95@users.noreply.github.com>
2023-07-17 18:31:13 +0800

    Add files via upload

---
+++ file_directory_correction.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 18:16:10 2023
+
+@author: monk
+"""
+import os
+
+def mkdir(path):
+ 
+    folder = os.path.exists(path)
+ 
+    if not folder:                   
+        os.makedirs(path)            
+        print ("---  The file directory is correct...  ---")
+ 
+    else:
+        print ("---  The file directory has been created correctly!  ---")
+
+data_id = [3, 4, 7, 8]
+for i in data_id:
+    file_accuracy = "./save/PEMS0{}/accuracy/".format(i)
+    file_data = "./save/PEMS0{}/data/".format(i)
+    file_log = "./save/PEMS0{}/log/".format(i)
+    file_loss = "./save/PEMS0{}/loss/".format(i)
+    mkdir(file_accuracy)
+    mkdir(file_data)
+    mkdir(file_log)
+    mkdir(file_loss)
